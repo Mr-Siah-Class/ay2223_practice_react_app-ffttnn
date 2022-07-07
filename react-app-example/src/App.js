218fd0f6a6d14b406f732f83467439a75621ef9d
Fiona <ftyx.19@ichat.sp.edu.sg>
2022-07-07 14:34:43 +0800

    creating react app #1

--- react-app-example/src/App.js
+++ react-app-example/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload HRLLOHLHELOO.
+          Edit <code>src/App.js</code> and save to reload HRLLOSSSHLHELOO.
         </p>
         <a
           className="App-link"
